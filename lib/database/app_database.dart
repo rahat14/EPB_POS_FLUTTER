b815be68763a87ec4d754f2ca434138a9a82fac0
Rahatul Aseaikin Shovo <rahatshovo6666@gmail.com>
2022-02-07 18:03:35 +0600

    added getx , moor database

---
+++ lib/database/app_database.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+// here code will generate it self.
+part 'app_database.g.dart';
+
+class Products extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get name => text()();
+
+  TextColumn get desc => text()();
+
+  TextColumn get company => text()();
+
+  IntColumn get stock => integer()();
+}
+
+// This annotation tells the code generator which tables this DB works with
+@DriftDatabase(tables: [Products])
+// _$AppDatabase is the name of the generated class
+class AppDatabase extends _$AppDatabase {
+  AppDatabase()
+      // Specify the location of the database file
+      : super(_openConnection());
+
+  // Bump this when changing tables and columns.
+  // Migrations will be covered in the next part.
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'epb_pos_db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
